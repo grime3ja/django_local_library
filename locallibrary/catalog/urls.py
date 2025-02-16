d7d0edacc09403a176dce88b1de2c1fb678455a5
grime3ja <grime3ja@dukes.jmu.edu>
2025-02-16 14:44:44 -0500

    chapter 5 of mdn local library. static homepage

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
+urlpatterns = []
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]

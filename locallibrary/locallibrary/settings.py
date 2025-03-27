5ae7021f947aaa1a6e074e05b37db15d45bb2a89
grime3ja <grime3ja@dukes.jmu.edu>
2025-03-27 12:45:32 -0400

    static files

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "catalog", "static"),)
 
